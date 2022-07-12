372f1852afe498c913e7a09756730f2669449aa7
Nicolas Oliveira (Magnic0) <nicolsoliverg@gmail.com>
2022-07-12 18:38:04 -0300

    JS: re-organize code, separate by component type; add functions to validate inputs; create arrays to specify error messages

--- js/validacao.js
+++ js/validacao.js
@@ -1,12 +1,29 @@
 "use sctrict"
 
 const birthDate = document.querySelector('#nascimento');
-
-console.log(birthDate.value);
-
-birthDate.addEventListener('blur', (event) => {
-    valiDate(event.target);
-});
+const errorMessages = {
+    email: {
+        typeMismatch: 'Digite um email válido.',
+        valueMissing: 'O campo não pode estar vazio!'
+    },
+    password: {
+        patternMismatch: 'A senha deve conter ao menos 6 e no máximo 24 caracteres, com no mínimo uma letra maiúscula, uma minúscula e um número',
+        valueMissing: 'O campo não pode estar vazio!'
+    },
+    birthDate: {
+        customError: 'Você deve ser maior de 18 para se cadastrar!',
+        valueMissing: 'O campo não pode estar vazio!'
+    }
+};
+const validators = {
+    birthDate:input => valiDate(input)
+};
+const errorTypes = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+];
 
 export function validate(input) {
     const inputType = input.dataset.type;
@@ -14,10 +31,26 @@ export function validate(input) {
     if(validators[inputType]) {
         validators[inputType](input);
     };
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+    } else {
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = showErrorMsg(inputType, input);
+    };
 }
 
-const validators = {
-    birthDate:input => valiDate(input)
+function showErrorMsg(inputType, input) {
+    let msg = '';
+
+    errorTypes.forEach(event => {
+        if(input.validity[event]) {
+            msg = errorMessages[inputType][event];
+        }
+    });
+
+    return msg;
 }
 
 function valiDate(input) {
@@ -41,3 +74,7 @@ function moreThan18(date) {
 
     return (moreThan18Date <= currentDate);
 }
+
+birthDate.addEventListener('blur', (event) => {
+    valiDate(event.target);
+});
