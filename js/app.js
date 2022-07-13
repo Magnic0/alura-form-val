f61815bf32a8e27b89a08adb8a3f04a072ee96d9
Nicolas Oliveira (Magnic0) <nicolsoliverg@gmail.com>
2022-07-13 12:55:53 -0300

    JS: fix (birthDate is null) error

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 import { validate } from "./validacao.js";
 
 const inputs = document.querySelectorAll('input');
+const birthDate = document.querySelector('#nascimento');
 
 inputs.forEach((input) => {
     input.addEventListener('blur', (event) => {
         validate(event.target);
     })
+    if(birthDate !== null) {
+        birthDate.addEventListener('blur', (event) => {
+            valiDate(event.target);
+        });
+    }
 })
