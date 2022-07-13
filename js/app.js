bfc30ca533debb5db0a6dd9141a60aed9cf17d9c
Nicolas Oliveira (Magnic0) <nicolsoliverg@gmail.com>
2022-07-13 13:47:20 -0300

    JS: implement simple mask money feature to preco input field

--- js/app.js
+++ js/app.js
@@ -4,9 +4,25 @@ const inputs = document.querySelectorAll('input');
 const birthDate = document.querySelector('#nascimento');
 
 inputs.forEach((input) => {
+
     input.addEventListener('blur', (event) => {
         validate(event.target);
+        if(input.dataset.type === 'price') {
+            if(input.value == 0) {
+                return;
+            }
+            SimpleMaskMoney.setMask(input, {
+                allowNegative: false,
+                prefix: 'R$ ',
+                fixed: true,
+                fractionDigits: 2,
+                decimalSeparator: ',',
+                thousandsSeparator: '.',
+                cursor: 'end'
+            })
+        }
     })
+
     if(birthDate !== null) {
         birthDate.addEventListener('blur', (event) => {
             valiDate(event.target);
